eb40184723937cb15868c6160144e00ba061fad6
LucasChimirri <84354246+LucasChimirri@users.noreply.github.com>
2025-03-23 19:11:04 -0300

    Update GreetingControllerTests.java
    Signed-off-by: LucasChimirri <84354246+LucasChimirri@users.noreply.github.com>

--- complete/src/test/java/com/example/restservice/GreetingControllerTests.java
+++ complete/src/test/java/com/example/restservice/GreetingControllerTests.java
@@ -26,11 +26,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-/*
+	/*
 	@SpringBootTest: Carrega todo o contexto do Spring Boot para testes de integração.
 
 @AutoConfigureMockMvc: Configura o MockMvc para testes de controladores.
-	*\
+	*/
 @SpringBootTest
 @AutoConfigureMockMvc
 public class GreetingControllerTests {
@@ -39,7 +39,7 @@ public class GreetingControllerTests {
 	@Autowired: Injeta dependências automaticamente.
 
 MockMvc: Simula requisições HTTP e respostas para testar controladores.
-	*\
+	*/
 	@Autowired
 	private MockMvc mockMvc;
 
